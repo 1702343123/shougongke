3d4d1fff19491f65e95f675395d6914bb4954df3
Joker <2635225112@qq.com>
2020-05-27 01:58:56 +0800

    搭建项目

---
+++ src/main/java/cn/niit/shougongke/ShougongkeApplication.java
@@ -0,0 +1,13 @@
+package cn.niit.shougongke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShougongkeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShougongkeApplication.class, args);
+    }
+
+}
